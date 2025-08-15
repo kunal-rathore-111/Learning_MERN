3aed1fa08b1bcacab05e08de9084f4d17fdb84c2
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-08-15 14:33:54 +0530

    adding lecture code files of week 7.2 adn 8.1 done in last 7days bw 7Aug to 13Aug25

--- Cohort 3.0/7.2/index.js
+++ Cohort 3.0/7.2/index.js
@@ -1,3 +1,7 @@
+// 1-> Password hasing (bcrypt)
+// 2-> Error handling (try catch)
+// 3-> Input validation (zod)    assignment -> go through zod library and find how to check uppercase lowercase and all
+
 
 const express = require("express");
 const { signINauth, auth } = require("./auth.js");
@@ -5,6 +9,7 @@ const mongoose = require("mongoose");
 const { usersModel, todosModel } = require("./db.js");
 
 const bcrypt = require("bcrypt"); // importing bcrypt library
+const { z } = require("zod"); //  importing zod library
 
 mongoose.connect("mongodb+srv://kunaldell01:123@cluster0.0qgphf9.mongodb.net/todo-1");
 
@@ -13,6 +18,28 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+
+
+    // zod->
+    // defining zod schema
+    const requiredStructure = z.object({
+        username: z.string().min(3).max(80),
+        password: z.string().min(3).max(80),
+        email: z.email().min(5).max(60) // checks whether the email length bw 5 to 60 and valid email format
+    });
+
+    // using that structure to validate 
+    const zodResponse = requiredStructure.safeParse(req.body);   // returns two things one is 'success' and 'error', here we can use .parse but it will 'throw' error unless giving as object as safeParse do 
+
+    //then if not succedded then return
+    if (!zodResponse.success) {
+        res.json({
+            message: "Invalid credentials",
+            error: zodResponse.error // responsing the wrong thing 
+        });
+        return;
+    }
+
 
     // error handling if there is any error like duplicate email or username etc.
     try {
