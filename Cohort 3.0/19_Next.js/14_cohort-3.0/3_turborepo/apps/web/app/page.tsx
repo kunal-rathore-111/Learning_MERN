2bd08b151855c34f318299af99d6341ff26f0996
Kunal Rathore <kunalworkspace111@gmail.com>
2026-02-27 22:04:21 +0530

    exploring turbopack, switching system form hp to dell

---
+++ Cohort 3.0/19_Next.js/14_cohort-3.0/3_turborepo/apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function () {
+
+
+  return <div className="min-w-screen min-h-screen p-10 flex justify-center items-center">
+    <input type="text" />
+    <button>Join</button>
+  </div>
+}
